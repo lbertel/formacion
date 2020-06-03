eb2e7c1a1ff178f7e9e0a3e5b18ee82d26a38970
Luis Bertel <lbertel@gmail.com>
2020-06-03 11:38:55 -0500

    Adición de la estructura base

---
+++ src/test/java/formacion/FormacionApplicationTests.java
@@ -0,0 +1,13 @@
+package formacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
